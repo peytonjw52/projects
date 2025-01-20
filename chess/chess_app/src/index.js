a79626403a9bfd65872f8665b19a3d2eeb759049
Peyton Worthington <151202707+peytonjw52@users.noreply.github.com>
2025-01-19 23:29:00 -0500

    Updated to import the app file

--- chess/chess_app/src/index.js
+++ chess/chess_app/src/index.js
@@ -1,80 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
-import board from './board.jpg'
-import {Board, Pawn, Bishop, Knight, Rook, Queen, King, Coordinate, handleButtonClick} from './pieces.ts'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Game from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-function Buttons() {
-  // Create the chessboard
-  const rows = 8;
-  const columns = 8;
-
-  const chessboard = [];
-  for (let row = 0; row < rows; row++) {
-    for (let col = 0; col < columns; col++) {
-      const positionCoordinate = new Coordinate(row, col);
-      const position = `${String.fromCharCode(65 + col)}${row + 1}`; // Example: A1, B2, etc.
-      chessboard.push(
-        <button
-          key={position}
-          onClick={() => handleButtonClick(position)}
-          style={{
-            position: 'absolute',
-            left: `${385+88*row}px`,
-            top: `${60+88*col}px`,
-            width: '88px',
-            height: '88px',
-            backgroundColor: (row + col) % 2 === 0 ? '#f0d9b5' : '#b58863', // Light and dark squares
-          }}
-        >
-          {position}
-        </button>
-      );
-    }
-  }
-
-  return (
-    <div>
-      {chessboard}
-    </div>
-  );
-}
-
-function Image() {
-  return (
-    <div>
-      <img 
-        src={board} 
-        alt = 'Piece' 
-        style={{ position: 'absolute', left: '325px', width: '825px', height: 'auto'}} />
-    </div>
-  );
-}
-
-function renderPieces(board) {
-  const pieces = [];
-  for (let piece of board.getPieces()) {
-      pieces.push(
-      <img 
-        src={piece.icon} 
-        alt = 'Chessboard' 
-        style={{ 
-          position: 'absolute', 
-          top: `0px`,
-          left: '325px', 
-          width: '825px', 
-          height: 'auto'}} />
-    );
-  }
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Buttons />
-    <Image />
+    <Game />
   </React.StrictMode>
 );
 
